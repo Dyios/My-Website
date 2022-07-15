033b75b2b52718689a82df7f25846fd180a1e3dd
TOLBA Rafik <47982363+Dyios@users.noreply.github.com>
2022-07-15 22:00:51 +0100

    added contact page & updated profil image

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+    $('#name').keyup(function (event) {
+        var textBox = event.target;
+        var start = textBox.selectionStart;
+        var end = textBox.selectionEnd;
+        textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1).toLowerCase();
+        textBox.setSelectionRange(start, end);
+    });
+});
+jQuery(document).ready(function ($) {
+    $('#object').keyup(function (event) {
+        var textBox = event.target;
+        var start = textBox.selectionStart;
+        var end = textBox.selectionEnd;
+        textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1).toLowerCase();
+        textBox.setSelectionRange(start, end);
+    });
+});
